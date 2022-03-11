1bdd963f1543ab0a3711df96603edbc0bc01a902
Luka Žugaj <luka.zugaj7@gmail.com>
2022-03-11 09:09:02 +0100

    B

--- src/com/luv2code/Main.java
+++ src/com/luv2code/Main.java
@@ -3,6 +3,10 @@ package com.luv2code;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int a = 1;
+
+        System.out.println("Number: " + a);
+
+        System.out.println("Commit B");
     }
 }
